98447be77ce1533b89c0f4f0d0033b12ae8f9d54
junqiwu02 <junqiwu02@gmail.com>
2019-03-02 09:36:36 -0500

    added fancy graphics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FishDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FishDetector.java
@@ -5,6 +5,7 @@ import com.disnodeteam.dogecv.detectors.DogeCVDetector;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
@@ -23,15 +24,17 @@ public class FishDetector extends DogeCVDetector {
 
         Mat bitmask = new Mat();
 
-        // Core.inRange(rgba, new Scalar(127, 50, 0), new Scalar(255, 127, 50), bitmask);
-        Core.inRange(rgba, new Scalar(233, 0, 0, 0), new Scalar(255, 150, 122, 255), bitmask);
+        Core.inRange(rgba, new Scalar(220, 0, 0, 0), new Scalar(255, 160, 130, 255), bitmask);
 
         ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
         Imgproc.findContours(bitmask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         if(contours.isEmpty()) {
-            return bitmask;
+            Imgproc.line(rgba, new Point(0, rgba.height() / 2), new Point(rgba.width(), rgba.height() / 2), new Scalar(0, 255, 0));
+            Imgproc.line(rgba, new Point(rgba.width() / 2, 0), new Point(rgba.width() / 2, rgba.height()), new Scalar(0, 255, 0));
+
+            return rgba;
         }
 
         MatOfPoint largest = contours.get(0);
@@ -48,7 +51,12 @@ public class FishDetector extends DogeCVDetector {
         x = m.m10 / m.m00 / rgba.width() - 0.5;
         y = m.m01 / m.m00 / rgba.height() - 0.5;
 
-        return bitmask;
+        Mat dst = new Mat();
+        rgba.copyTo(dst, bitmask);
+        Imgproc.line(dst, new Point(0, rgba.height() / 2), new Point(rgba.width(), rgba.height() / 2), new Scalar(0, 255, 0));
+        Imgproc.line(dst, new Point(rgba.width() / 2, 0), new Point(rgba.width() / 2, rgba.height()), new Scalar(0, 255, 0));
+
+        return dst;
     }
     @Override
     public void useDefaults() {
